3d34bb92cecf5431777b091d8022e299dd877696
AaizaSoltonoeva <55399942+AaizaSoltonoeva@users.noreply.github.com>
2019-10-03 21:12:49 +0600

    Delete 2h.py

--- 2h.py
+++
@@ -1,8 +0,0 @@
-N = int(input("enter the number of angles"))
-if N < 3:
- print ("Wrong! enter the number of angles")
-elif N > 1000000:
- print ("Wrong! enter the number of angles")
-else:
-  sum = 180 * (N- 2)
-  print(sum)
