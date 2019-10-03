f277da2c26c927c7e76c193497abb152f0b05bad
AaizaSoltonoeva <55399942+AaizaSoltonoeva@users.noreply.github.com>
2019-10-03 20:58:11 +0600

    Add files via upload

---
+++ 3d.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = 'Spring'
+c = 'Summer'
+d = 'Autumn'
+e = 'Winter'
+f = 'Wrong number'
+if  a>=3 and a<=5:
+ print(b)
+elif a>=6 and a<=8:
+ print(c)
+elif a>=9 and a<=11:
+ print(d)
+elif a==12 or a==1 or a==2:
+ print(e)
+else:
+ print(f)
